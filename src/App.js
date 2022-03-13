8e5d44a1b10d965f00db19c7c68f2e91d137c157
fawriid <farid.miri.1393@gmail.com>
2022-03-13 16:43:08 +0330

    Login component UI created, ant design/icon pack installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import { Navigate, Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+// icons
+import { FacebookFilled, GoogleOutlined, TwitterOutlined } from "@ant-design/icons";
+
+// styles
+import styles from "./Login.module.css";
+
+const Login = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.cardContainer}>
+                <div className={styles.intro}>
+                    <h2>
+                        Wellcome to <span>Swap</span> Messenger <span>!</span>
+                    </h2>
+                </div>
+                <div className={styles.option}>
+                    <span>Sign in with Google </span>
+                    <GoogleOutlined className={styles.logo} />
+                </div>
+                <div className={styles.option}>
+                    <span>Sign in with Twitter </span>
+                    <TwitterOutlined className={styles.logo} />
+                </div>
+                <div className={styles.option}>
+                    <span>Sign in with Facebook </span>
+                    <FacebookFilled className={styles.logo} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
